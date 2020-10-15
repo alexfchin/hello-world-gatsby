5df738c7be9aeb4d22dc8dd17ae55fc446b5d8ce
Alex <alexchin@Alexs-MBP.fios-router.home>
2020-10-15 19:00:09 -0400

    init

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,13 @@
 import React from "react"
+import { Link } from "gatsby"
+import Header from "../components/header"
 
-export default function Home() {
-  return <div>Hello world!</div>
+export default function Contact() {
+  return (
+    <div style={{ color: `teal` }}>
+      <Link to="/">Home</Link>
+      <Header headerText="Contact" />
+      <p>Send us a message!</p>
+    </div>
+  )
 }
